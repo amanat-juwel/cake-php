c0595f08bf80659953a474b10677fa6a40980c07
Amanat <amanatjuwel@gmail.com>
2019-09-07 18:49:28 +0600

    route, controllers, view basic

---
+++ src/Controller/CustomController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+
+
+class CustomController extends AppController
+{
+
+
+    public function index()
+    {
+        //method-1
+        $title = "Custom Page Index";
+        $this->set('title',$title);
+
+        //method-2
+        $data = [
+            'language' => 'PHP',
+            'framework' => 'Cake PHP'
+        ];        
+
+        $this->set(compact("data"));
+
+
+    }
+
+    public function aboutUs()
+    {
+        //$this->autoRender = false;
+
+        $title = "About Us";
+        $this->set('title',$title);
+
+    }
+
+    public function contactUs($arg1 = '', $arg2 = '')
+    {
+        
+        $data = [
+            'param_1' => $arg1,
+            'param_2' => $arg2
+        ];   
+
+        $this->set(compact("data"));
+
+    }
+}
